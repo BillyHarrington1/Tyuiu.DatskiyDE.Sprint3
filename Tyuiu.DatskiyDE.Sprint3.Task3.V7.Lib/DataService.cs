89d9e235d4aadb08a9d984226cefd33e495a20b7
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-04 04:12:40 +0500

    Task3

---
+++ Tyuiu.DatskiyDE.Sprint3.Task3.V7.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DatskiyDE.Sprint3.Task3.V7.Lib
+{
+    public class DataService : ISprint3Task3V7
+    {
+        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
+        {
+            List<char> output = new List<char>(); //вместо ds. 
+
+            foreach (char c in value)
+            {
+                if (c == replaceable)
+                    output.Add(replacement);
+                else
+                    output.Add(c);
+            }
+
+            return new string(output.ToArray());
+        }
+    }
+}

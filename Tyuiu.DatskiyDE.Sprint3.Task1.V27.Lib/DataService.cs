dd8f118eaf0e78071c54e70f4884401a6435c24e
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-04 03:28:37 +0500

    Task1

---
+++ Tyuiu.DatskiyDE.Sprint3.Task1.V27.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DatskiyDE.Sprint3.Task1.V27.Lib
+{
+    public class DataService : ISprint3Task1V27
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+
+            double n = 0.75;
+            double productSeries = 1.0;
+
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                double term = Math.Pow(1.0 / Math.Pow(n, k), 3);
+                productSeries *= term;
+            }
+
+            return Math.Round(productSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint3.Task1.V27/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.DatskiyDE.Sprint3.Task1.V27.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint3.Task1.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Дацкий Д. Е. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: while                                                            *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #27                                                            *");
+            Console.WriteLine("* Выполнил: Дацкий Денис Евгеньевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу используя цикл while,                               *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле,при n = 0,75            *");
+            Console.WriteLine("*       5    1                                                           *");
+            Console.WriteLine("* p =   П  (---)^3                                                       *");
+            Console.WriteLine("*      k=1  n^k                                                          *");
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт = " + startValue);
+            Console.WriteLine("Конец = " + stopValue);
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+        }
+    }
+}

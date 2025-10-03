aa3d0ddb180bc3da90480ab687b08fd4c4340168
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-04 03:41:55 +0500

    Task2

---
+++ Tyuiu.DatskiyDE.Sprint3.Task2.V21.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DatskiyDE.Sprint3.Task2.V21.Lib
+{
+    public class DataService : ISprint3Task2V21
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0.0;
+            int k = startValue;
+
+            do
+            {
+                double Fimoz = Math.Sin(k) + 2 * Math.Pow(value, k);
+                double term = Math.Pow(1.0 / Fimoz, k);
+                sum += term;
+
+                k++;
+            }
+            while (k <= stopValue);
+
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint3.Task2.V21/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.DatskiyDE.Sprint3.Task2.V21.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint3.Task2.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Дацкий Д. Е. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: do...while                                                       *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #21                                                            *");
+            Console.WriteLine("* Выполнил: Дацкий Денис Евгеньевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу используя цикл do...while,                          *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при x = 1.5                   *");
+            Console.WriteLine("*       10   (    1     )^k                                              *");
+            Console.WriteLine("* S =   Σ   ( -------- )                                                 *");
+            Console.WriteLine("*      k=1   ( sin k + 2x^k )                                            *");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double value = 1.5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("x = " + value);
+            Console.WriteLine("Start = " + startValue);
+            Console.WriteLine("Stop = " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            double result = ds.GetSumSeries(value, startValue, stopValue);
+            Console.WriteLine("Сумма ряда S = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}

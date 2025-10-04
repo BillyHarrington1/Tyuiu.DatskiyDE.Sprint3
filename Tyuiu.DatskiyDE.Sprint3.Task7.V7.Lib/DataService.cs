7619122f004b9f4d469a1a138fc7ff31f089f821
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-04 17:27:48 +0500

    Task7

---
+++ Tyuiu.DatskiyDE.Sprint3.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DatskiyDE.Sprint3.Task7.V7.Lib
+{
+    public class DataService : ISprint3Task7V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] result = new double[len];
+            int index = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double fx = 0;
+
+                if (x + 2 == 0) 
+                {
+                    fx = 0;
+                }
+                else
+                {
+                    double cosX = Math.Cos(x);
+                    fx = cosX + (cosX / (x + 2)) - 3 * x;
+                }
+
+                result[index] = Math.Round(fx, 2); 
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint3.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.DatskiyDE.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint3.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double[] result = ds.GetMassFunction(startValue, stopValue);   
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(11, result.Length); 
+        }
+    }
+}
